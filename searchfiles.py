6f38419d9928d56d81998dad50e03d260a9b4927
drvinceknight <vincent.knight@gmail.com>
2013-09-28 18:47:17 +0100

    can trim files with -t tag

--- searchfiles.py
+++ searchfiles.py
@@ -6,10 +6,27 @@ import argparse
 import matplotlib.pyplot as plt
 import pickle
 
+def trim(t, p=0.01):
+    """Trims the largest and smallest elements of t.
+
+    Args:
+    t: sequence of numbers
+    p: fraction of values to trim off each end
+
+    Returns:
+    sequence of values
+    """
+    t.sort()
+    n = int(p * len(t))
+    t = t[n:-n]
+    return t
+
 parser = argparse.ArgumentParser(description="A simple script to find word counts of all tex files in all subdirectories of a target directory.")
 parser.add_argument("directory", help="the directory you would like to search")
+parser.add_argument("-t", "--trim", help="trim data percentage", default=0)
 args = parser.parse_args()
 directory = args.directory
+p = float(args.trim)
 
 matches = []
 for root, dirnames, filenames in os.walk(directory):
@@ -32,11 +49,16 @@ for f in matches:
 
 pickle.dump(wordcounts, open('latexwordcountin%s.pickle' % directory.replace("/", "-"), "w"))
 
-data = [wordcounts[e] for e in wordcounts]
 
-plt.figure()
-plt.hist(data, bins=20)
-plt.xlabel("Words")
-plt.ylabel("Frequency")
-plt.title("Distribution of words counts in all my LaTeX documents\n ($N=%s$,mean=$%s$, max=$%s$)" % (len(data), sum(data)/len(data), max(data)))
-plt.savefig('latexwordcountin%s.svg' % directory.replace("/", "-"))
+try:
+    data = [wordcounts[e] for e in wordcounts]
+    data = trim(data, p)
+
+    plt.figure()
+    plt.hist(data, bins=20)
+    plt.xlabel("Words")
+    plt.ylabel("Frequency")
+    plt.title("Distribution of words counts in all my LaTeX documents\n ($N=%s$,mean=$%s$, max=$%s$)" % (len(data), sum(data)/len(data), max(data)))
+    plt.savefig('latexwordcountin%s.svg' % directory.replace("/", "-"))
+except:
+    print "Graph not produced, perhaps you don't have matplotlib installed..."
